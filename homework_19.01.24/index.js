7acb2377f1e8c1d6a103abac00d71acd3c799719
Dell Latitude E5570 <hhuseyn573@gmail.com>
2024-01-21 20:45:59 +0400

    3 tasks compeletly

--- homework_19.01.24/index.js
+++ homework_19.01.24/index.js
@@ -34,27 +34,72 @@
 // 2. solda duran element sagdaki elementden boyukdurse solda duran novbeti elementlere baxir ve sonra egelnden cixir ferqi yeni Arraya push edir
 // 3. eger solda duran element sagdakilarin hamisindan boyukdurse o zaman hemin elementin ozunu push edir
 
-var array = [8, 6, 4, 3, 4];
-function differenceReturnAry(ary) {
-  var newAry = [];
-  var indx = false;
-  for (let i = 0; i < ary.length; i++) {
-    for (let f = i; f < ary.length; f++) {
-      indx = true;
-      if (ary[i] > ary[f]) {
-        newAry.push(ary[i] - ary[f]);
-        indx = false;
-        break;
-      }
-    }
-    if (indx) {
-      newAry.push(ary[i]);
-      indx = false;
-    }
-  }
-  return newAry;
-}
+// var array = [8, 6, 4, 3, 4];
+// function differenceReturnAry(ary) {
+//   var newAry = [];
+//   var indx = false;
+//   for (let i = 0; i < ary.length; i++) {
+//     for (let f = i; f < ary.length; f++) {
+//       indx = true;
+//       if (ary[i] > ary[f]) {
+//         newAry.push(ary[i] - ary[f]);
+//         indx = false;
+//         break;
+//       }
+//     }
+//     if (indx) {
+//       newAry.push(ary[i]);
+//       indx = false;
+//     }
+//   }
+//   return newAry;
+// }
 
-//---------------------
+// //---------------------
 
-console.log(differenceReturnAry(array));
+// console.log(differenceReturnAry(array));
+//=====================================================================================
+// // murekkeb ededleri arryde qaytaran array
+// var inptNumber = prompt("Enter nmbr");
+
+// function isPrimeNmbr(nmbr) {
+//   var result = false;
+
+//   for (let i = 2; i < nmbr; i++) {
+//     if (nmbr % i == 0) {
+//       result = true;
+//       break;
+//     }
+//   }
+
+//   return result;
+// }
+// function primeNumbrArry(nmber) {
+//   var newAry = [];
+//   for (let i = 2; i <= nmber; i++) {
+//     if (isPrimeNmbr(i)) {
+//       newAry.push(i);
+//     }
+//   }
+//   return newAry;
+// }
+// console.log(primeNumbrArry(inptNumber));
+
+//======================================================
+// arrayda reqem sayi 2ye bolunen ededlerinin sayi
+// Task 2 during the lesson
+
+// var arry = [122, 2132, 123, 23, 5566, 669988];
+
+// function countNumber(ary) {
+//   var count = 0;
+
+//   for (let i = 0; i < ary.length; i++) {
+//     if (ary[i].toString().length % 2 == 0) {
+//       count += 1;
+//     }
+//   }
+//   return count;
+// }
+// console.log(countNumber(arry));
+//=====================================================
