5b210278d9333a5d0fee49d2a7380fe67cf964dc
Dell Latitude E5570 <hhuseyn573@gmail.com>
2024-03-02 20:57:19 +0400

    CRUD operations some last correct

--- HomeWork_Short/task_28.02.24_CRUD_opertns/indx.js
+++ HomeWork_Short/task_28.02.24_CRUD_opertns/indx.js
@@ -2,18 +2,25 @@
 
 let users = [
   {
-    id: 0,
+    id: 1,
     name: "Huseyn",
     surname: "Huseynov",
     age: 30,
   },
   {
-    id: 1,
+    id: 2,
     name: "Hesen",
     surname: "Hesenov",
     age: 31,
   },
+  {
+    id: 3,
+    name: "Abdul",
+    surname: "Musayev",
+    age: 31,
+  },
 ];
+
 
 function addUser(id, name, surname, age) {
   this.id = id;
@@ -21,8 +28,8 @@ function addUser(id, name, surname, age) {
   this.surname = surname;
   this.age = age;
 }
-let getUser = (userId, data) => {
-  let findUser = data.find((elm) => elm.id == userId);
+let getUser = (userId) => {
+  let findUser = users.find((elm) => elm.id == userId);
   if (!findUser) {
     return {
       success: false,
@@ -31,12 +38,13 @@ let getUser = (userId, data) => {
   }
 
   return {
+    success: true,
     massage: "User found",
     data: findUser,
   };
 };
-let removeUser = (userId, data) => {
-  let findUserIndex = data.findIndex((elm) => elm.id == userId);
+let removeUser = (userId) => {
+  let findUserIndex = users.findIndex((elm) => elm.id == userId);
 
   if (findUserIndex == -1) {
     return {
@@ -45,33 +53,33 @@ let removeUser = (userId, data) => {
     };
   }
 
-  data.splice(findUserIndex, 1);
+  let result = users.filter((elm) => elm.id !== userId);
 
   return {
     success: true,
     massage: "User successfully deleted",
+    data: result,
   };
 };
 
-let updateUserFoo = (userId, data, name, surname, age) => {
-  let findUserIndex = data.findIndex((elm) => elm.id == userId);
+let updateUser = (userId,update) => {
+  let findUser = users.find((elm) => elm.id == userId);
 
-  if (findUserIndex == -1) {
+  if (!findUser) {
     return {
       success: false,
       massage: "Not found User Id",
     };
   }
-  let findUser = data[findUserIndex];
-
+  
   let { ...updateUsrElm } = { name: name, surname: surname, age: age };
 
   let updateUser = {
     ...findUser,
-    ...updateUsrElm,
+    ...update,
   };
 
-  data[findUserIndex] = updateUser;
+  findUser = updateUser;
 
   return {
     success: true,
@@ -80,47 +88,47 @@ let updateUserFoo = (userId, data, name, surname, age) => {
   };
 };
 let result = true;
-while (result) {
-  let getClientValue = prompt(`Enter number:
-1 ---- Add user;
-2 ---- Get user
-3 ---- Delete user
-4 ---- Update user
-5 ---- Exit program`);
-  switch (parseInt(getClientValue)) {
-    case 1:
-      users.push(
-        new addUser(
-          prompt("Enter ID"),
-          prompt("Enter Name"),
-          prompt("Enter surname"),
-          prompt("Enter age")
-        )
-      );
-      break;
-    case 2:
-      console.log(getUser(prompt("Enter user ID for Find"), users));
-      break;
-    case 3:
-      console.log(removeUser(prompt("Enter user ID for delete"), users));
-      break;
-    case 4:
-      console.log(
-        updateUserFoo(
-          prompt("Enter ID for update"),
-          users,
-          prompt("Enter user name"),
-          prompt("Enter user surname"),
-          prompt("Enter user age")
-        )
-      );
-      break;
-    case 5:
-      alert("Program is closing byee");
-      result = false;
-      break;
-    default:
-      alert("Wrong information");
-      break;
-  }
-}
+// while (result) {
+//   let getClientValue = prompt(`Enter number:
+// 1 ---- Add user;
+// 2 ---- Get user
+// 3 ---- Delete user
+// 4 ---- Update user
+// 5 ---- Exit program`);
+//   switch (parseInt(getClientValue)) {
+//     case 1:
+//       users.push(
+//         new addUser(
+//           prompt("Enter ID"),
+//           prompt("Enter Name"),
+//           prompt("Enter surname"),
+//           prompt("Enter age")
+//         )
+//       );
+//       break;
+//     case 2:
+//       console.log(getUser(prompt("Enter user ID for Find"), users));
+//       break;
+//     case 3:
+//       console.log(removeUser(prompt("Enter user ID for delete"), users));
+//       break;
+//     case 4:
+//       console.log(
+//         updateUserFoo(
+//           prompt("Enter ID for update"),
+//           users,
+//           prompt("Enter user name"),
+//           prompt("Enter user surname"),
+//           prompt("Enter user age")
+//         )
+//       );
+//       break;
+//     case 5:
+//       alert("Program is closing byee");
+//       result = false;
+//       break;
+//     default:
+//       alert("Wrong information");
+//       break;
+//   }
+// }
